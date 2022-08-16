3e83013f809737da965449687931fdd576dd0b91
Adevikki <vikkibabs@gmail.com" "Adevikki>
2022-08-16 15:02:50 +0100

    Home page flows with custom navigation flow

---
+++ lib/presentations/budget_items_list_screens/expenses_task_bottom_sheet/create_budget_screen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:trade_depot_tracker_assessment_test/core/widgets/app_text_field.dart';
+import 'package:trade_depot_tracker_assessment_test/core/widgets/button_widget.dart';
+import 'package:trade_depot_tracker_assessment_test/core/widgets/constants/select_text_field.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/colors.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/constants.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/styles.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/validations.dart';
+
+class CreateBudgetScreen extends ConsumerStatefulWidget {
+  const CreateBudgetScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<CreateBudgetScreen> createState() => _CreateBudgetScreenState();
+}
+
+class _CreateBudgetScreenState extends ConsumerState<CreateBudgetScreen> {
+  final _isCreateBudgetAmountInvalid = false;
+
+  final _createBudgetAmountController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // get the media query
+    final _mediaQuery = MediaQuery.of(context).size;
+    // final familyExpensesOptions = ref.watch(familyExpensesListFutureProvider);
+
+    return Scaffold(
+      backgroundColor: AppColors.buttonBlueColor,
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          // horizontal: 16,
+          vertical: 24,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 24,
+          ),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 30),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (Navigator.of(context).canPop()) {
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: SvgPicture.asset(
+                        '$kImagePath/back_arrow.svg',
+                        width: 24,
+                        height: 24,
+                        color: AppColors.black,
+                        semanticsLabel: 'Back',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 60),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Create a budget',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  AppTextField(
+                    labelText: 'Budget name',
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 0,
+                    ),
+                    backgroundColor: AppColors.bgColor,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: Validations.email,
+                    // enableErrorMessage: _isCreateBudgetAmountInvalid,
+                    controller: _createBudgetAmountController,
+                    hintText: '',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  AppTextField(
+                    labelText: 'Budget amount',
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 0,
+                    ),
+                    backgroundColor: AppColors.bgColor,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: Validations.email,
+                    // enableErrorMessage: _isCreateBudgetAmountInvalid,
+                    controller: _createBudgetAmountController,
+                    hintText: '',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectFieldWidget(
+                    hintText: 'Choose interval',
+                    // value: selectedNetwork.chain.isNotEmpty
+                    //     ? selectedNetwork.chain
+                    //     : null,
+                    // action: () {
+                    //   context.showBottomSheet(
+                    //     child: SelectCryptoNetworkBottomSheet(),
+                    //   );
+                    // },
+                  ),
+                  const SizedBox(
+                    height: 135,
+                  ),
+                  SizedBox(
+                    width: _mediaQuery.width,
+                    child: ButtonWidget(
+                        buttonText: 'Create Budget',
+                        buttonTextStyle: Styles.p2(
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                        action: () {
+                          // validate form
+                          // validateForm();
+                        }),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        height: _mediaQuery.height,
+        width: _mediaQuery.width,
+        decoration: const BoxDecoration(
+          color: Styles.trackerWhite,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentations/home/custom_navigation_screen.dart
@@ -0,0 +1,164 @@
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:trade_depot_tracker_assessment_test/presentations/home/menu/menu_screens/budgets_view.dart';
+import 'package:trade_depot_tracker_assessment_test/presentations/home/menu/menu_screens/categories_view.dart';
+import 'package:trade_depot_tracker_assessment_test/presentations/home/menu/menu_screens/home_view.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/constants.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/styles.dart';
+import 'package:trade_depot_tracker_assessment_test/utils/utils.dart';
+
+class CustomBottomNavigation extends ConsumerStatefulWidget {
+  static const pageId = 'bottom-navigation';
+
+  final int tabIndex;
+
+  const CustomBottomNavigation({
+    Key? key,
+    this.tabIndex = 0,
+  }) : super(key: key);
+
+  @override
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState
+    extends ConsumerState<CustomBottomNavigation> {
+  int? _selectedIndex = 0;
+  late Timer _timer;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    CategoriesScreen(),
+    BudgetsScreen(),
+  ];
+
+  void _onItemTapped(int? index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    _selectedIndex = widget.tabIndex;
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _widgetOptions.elementAt(_selectedIndex!),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            // border: const Border(
+            //   top: BorderSide(width: 0, color: Styles.trackerGrey500),
+            // ),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.8),
+                blurRadius: 2, // soften the shadow
+                spreadRadius: 0.5, //extend the shadow
+                offset: const Offset(
+                  2, // Move to right 10  horizontally
+                  2,
+                  // Move to bottom 10 Vertically
+                ),
+              )
+            ],
+          ),
+          height: isIos() ? 70 : 50,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 24,
+              right: 24,
+              top: 4,
+              bottom: isIos() ? 20 : 4,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                bottomBarMenu(
+                  context: context,
+                  image: _selectedIndex == 0
+                      ? '$kImagePath/home_selected.svg'
+                      : '$kImagePath/home.svg',
+                  selectIndex: 0,
+                  text: 'Home',
+                ),
+                bottomBarMenu(
+                  context: context,
+                  image: _selectedIndex == 1
+                      ? '$kImagePath/categories_selected.png'
+                      : '$kImagePath/categories.svg',
+                  selectIndex: 1,
+                  text: 'Categories',
+                ),
+                bottomBarMenu(
+                  context: context,
+                  image: _selectedIndex == 2
+                      ? '$kImagePath/budgets_selected.svg'
+                      : '$kImagePath/budgets.svg',
+                  selectIndex: 2,
+                  text: 'Budget',
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Material bottomBarMenu({
+    BuildContext? context,
+    String? image,
+    int? selectIndex,
+    String? text,
+  }) {
+    return Material(
+      color: Colors.transparent,
+      child: GestureDetector(
+        onTap: () {
+          _onItemTapped(selectIndex);
+        },
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                '$image',
+                width: 24,
+                height: 24,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                '$text',
+                style: Styles.smallNav(
+                  color: selectIndex == _selectedIndex
+                      ? Styles.trackerPurple600
+                      : Styles.trackerGrey500,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
